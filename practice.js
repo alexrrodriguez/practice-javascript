1e3bdf6f57ad521789f8593dc4ed12545e8a16ab
alex <arodriguez202020@gmail.com>
2021-07-12 13:25:41 -0500

    add new ruby to javascript problems

--- practice.js
+++ practice.js
@@ -42,14 +42,14 @@
 
 // Write a method that prints out every number from 1 to 100.
 
-function print() {
-  var num = 1;
-  while (num < 101) {
-    console.log(num);
-    num += 1;
-  }
-}
-print();
+// function print() {
+//   var num = 1;
+//   while (num < 101) {
+//     console.log(num);
+//     num += 1;
+//   }
+// }
+// print();
 
 // Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
 
@@ -110,15 +110,169 @@ print();
 // Write a method that accepts a number and returns its factorial.
 // For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 
-function fact(num) {
-  var product = 1;
-  while (num > 0) {
-    product *= num;
-    num -= 1;
-  }
-  console.log(product);
-}
-fact(5);
+// function fact(num) {
+//   var product = 1;
+//   while (num > 0) {
+//     product *= num;
+//     num -= 1;
+//   }
+//   console.log(product);
+// }
+// fact(5);
 
 // Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array.
 // For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+// function selectEvenItems(strings) {
+//   var result = [];
+//   var index = 0;
+//   strings.forEach(function (string) {
+//     if (index % 2 === 0) {
+//       result.push(string);
+//     }
+//     index = index + 1;
+//   });
+//   console.log(result);
+// }
+// selectEvenItems(["a", "b", "c", "d", "e", "f"]);
+
+// # Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+// function max(numbers) {
+//   var currentMax = numbers[0];
+//   numbers.forEach(function (number) {
+//     if (number > currentMax) {
+//       currentMax = number;
+//     }
+//   });
+//   console.log(currentMax);
+// }
+
+// max([5, 4, 8, 1, 2]);
+
+// # Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+// function factorial(number) {
+//   var result = number;
+//   while (number > 1) {
+//     number -= 1;
+//     result = result * number;
+//   }
+//   console.log(result);
+// }
+
+// factorial(5);
+
+// # Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+
+// function descending(numbers) {
+//   var newArray = [];
+//   var length = numbers.length;
+//   for (var i = 0; i < length; i++) {
+//     newArray.push(numbers.pop());
+//   }
+//   console.log(newArray);
+// }
+
+// descending([1, 3, 5, 7]);
+
+// # Write a method that accepts two arrays of numbers, and returns an array of every sum of every combination of single numbers from the first and second array. For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should return this array: [101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+// function sum_combos(num1, num2) {
+//   var result = [];
+//   num1.forEach(function (number1) {
+//     num2.forEach(function (number2) {
+//       result.push(number1 + number2);
+//     });
+//   });
+//   console.log(result);
+// }
+
+// sum_combos([1, 5, 10], [100, 500, 1000]);
+
+// # Write a function that takes in an array of numbers and returns its sum.
+
+// function sum(num) {
+//   var sum = 0;
+//   num.forEach(function (number) {
+//     sum += number;
+//   });
+//   console.log(sum);
+// }
+// sum([1, 2, 3, 4, 5]);
+
+// # Write a function that takes in an array of strings and returns the smallest string.
+
+// function smallest(array) {
+//   var small = array[0];
+//   var i = 0;
+//   while (i < array.length) {
+//     if (small.length > array[i].length) {
+//       small = array[i];
+//     }
+//     i += 1;
+//   }
+//   console.log(small);
+// }
+
+// smallest(["cat", "bird", "llama", "horse"]);
+
+// Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+
+// function rev(array) {
+//   var newArray = [];
+//   var i = 0;
+//   while (i < array.length) {
+//     newArray.push(array.pop());
+//   }
+//   i += 1;
+//   console.log(newArray);
+// }
+// rev([5, 9, 8, 3, 2]);
+
+// # Write a function that takes in an array of words and returns the number of words that begin with the letter “a”.
+
+// function aWords(array) {
+//   var i = 0;
+//   var count = 0;
+//   while (i < array.length) {
+//     if (array[i][0] === "a") {
+//       count += 1;
+//     }
+//     i += 1;
+//   }
+//   console.log(count);
+// }
+
+// aWords(["alligator", "dog", "fox", "armadillo"]);
+
+// Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+
+// function combine(array) {
+//   var i = 0;
+//   var str = "";
+//   while (i < array.length - 1) {
+//     str += array[i] + ",";
+//     i += 1;
+//   }
+//   str += array[array.length - 1];
+//   console.log(str);
+// }
+
+// combine(["cat", "dog", "snake", "mouse"]);
+
+// # Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+
+function product(array) {
+  var pro = 1;
+  var i = 0;
+  while (i < array.length) {
+    pro = pro * array[i];
+    i += 1;
+  }
+  console.log(pro);
+}
+
+product([2, 3, 4, 5]);
+
+// Write a function that takes in an array of numbers and returns the two smallest numbers.
--- practice.rb
+++ practice.rb
@@ -47,14 +47,14 @@
 
 # Write a method that prints out every number from 1 to 100. 
 
-def print
-  num = 1
-  while num < 101
-    p num
-    num += 1
-  end
-end
-print
+# def print
+#   num = 1
+#   while num < 101
+#     p num
+#     num += 1
+#   end
+# end
+# print
 
 # Write a method that prints out every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
 
@@ -122,17 +122,181 @@ print
 # Write a method that accepts a number and returns its factorial.
 # For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 
-def fact (num)
-  product = 1
-  while num > 0
-    product *= num
-    num -= 1
-  end
-  return product
-end
+# def fact (num)
+#   product = 1
+#   while num > 0
+#     product *= num
+#     num -= 1
+#   end
+#   return product
+# end
 
-puts fact(5)
+# puts fact(5)
 
 
 # Write a method that accepts two arrays of numbers, and prints the sum of every combination of numbers from first and second array. 
 # For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should print a list: 101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+# Write a function that accepts an array of strings and returns a new array containing every other string from the original array. For example, if the input is ["a", "b", "c", "d", "e", "f"], the output should be ["a", "c", "e"].
+
+# def select_even_items(strings)
+#     result = []
+#     index = 0
+#     strings.each do |string|
+#       if index % 2 == 0
+#         result << string
+#       end
+#       index = index + 1
+#     end
+#     result
+#   end
+#   p select_even_items(["a", "b", "c", "d", "e", "f"])
+
+# Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
+
+# def max(numbers)
+#     current_max = numbers[0]
+#     numbers.each do |number|
+#       if number > current_max
+#         current_max = number
+#       end
+#     end
+#     current_max
+#   end
+#   p max([5, 4, 8, 1, 2])
+
+  # Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+# def factorial(number)
+#     result = 1
+#     current_number = number
+#     number.times do
+#       result = result * current_number
+#       current_number = current_number - 1
+#     end
+#     result
+#   end
+#   p factorial(5)
+
+# Write a method that accepts one argument - an array of numbers that are in ascending order. The method that returns a new array with the same values in descending order. However, do not use the "reverse" method built into Ruby.
+
+# def descending(numbers)
+#     result = []
+#     index = numbers.length - 1
+#     numbers.length.times do
+#       result << numbers[index]
+#       index = index - 1
+#     end
+#     result
+#   end
+#   p descending([1, 3, 5, 7])
+
+# Write a method that accepts two arrays of numbers, and returns an array of every sum of every combination of single numbers from the first and second array. For example, if the method receives [1, 5, 10] and [100, 500, 1000], the method should return this array: [101, 501, 1001, 105, 505, 1005, 110, 510, 1010].
+
+# def sum_combinations(numbers1, numbers2)
+#     result = []
+#     numbers1.each do |number1|
+#       numbers2.each do |number2|
+#         result << number1 + number2
+#       end
+#     end
+#     result
+#   end
+#   p sum_combinations([1, 5, 10], [100, 500, 1000])
+
+# Write a function that takes in an array of numbers and returns its sum.
+
+# def sum(num) 
+#   sum = 0
+#   num.each do |number|
+#     sum += number
+#   end
+#   p sum
+# end
+
+# sum([1,2,3,4,5])
+
+# Write a function that takes in an array of strings and returns the smallest string.
+
+# def smallest(array)
+#   small = array[0]
+#   i = 0
+#   while i < array.length
+#     if small.length > array[i].length
+#       small = array[i]
+#     end
+#     i += 1
+#   end
+#   p small
+# end
+
+# smallest(["cat", "bird", "llama", "horse"])
+
+# Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+
+# def rev(array)
+#   new_array = []
+#   i = 0
+#   while i < array.length
+#     new_array << array.pop
+#   end
+#   i += 1
+#   p new_array
+# end
+
+# rev([5, 9, 8, 3, 2])
+
+# Write a function that takes in an array of words and returns the number of words that begin with the letter “a”.
+
+# def a_words(array)
+#   i = 0
+#   count = 0
+#   while i < array.length
+#     if array[i][0] == "a"
+#       count += 1
+#     end
+#     i += 1
+#   end
+#   p count
+# end
+
+# a_words(["alligator", "dog", "fox", "armadillo"])
+
+# Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+
+# def combine(array)
+#   i = 0
+#   str = ""
+#   while i < array.length - 1
+#       str += array[i] + ","
+#       i += 1
+#   end
+#   str += array.last
+#   p str
+# end
+
+# combine(["cat", "dog", "snake", "mouse"])
+
+# Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other). 
+
+def product(array)
+  pro = 1
+  i = 0
+  while i < array.length
+    pro = pro * array[i]
+    i += 1
+  end
+  p pro
+end
+
+product([2, 3, 4, 5])
+
+# # Write a function that takes in an array of numbers and returns the two smallest numbers.
+
+# def two_small(array)
+#   i = 0
+#   while i < array.length
+#     if array[i] > array[i + 1]
+# end
+
+# two_small([4, 8, 1, 5, 2])
